bb3f9bf115950044fc7b6815a14e15518c2ec677
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-08-09 16:30:46 -0500

    Update main.py

--- video/main.py
+++ video/main.py
@@ -1,30 +1,33 @@
+from multiprocessing import Process
 from PIL import ImageGrab
 import cv2
-import threading
 import win32con
 import win32gui
 import pyautogui
-import os
 import time
+import pathlib
 import scan
 
-THIS_PATH = os.getcwd()
+PATH = str(pathlib.Path(__file__).parent.resolve())
 START_TIME = time.time()
-WINDOW_NAME = '镜中人'
+WINDOW_NAME = 'man in mirror'
 
 
 def window_capture() -> None:
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(THIS_PATH+'\\'+r'%s.png' % str(time.time()-START_TIME))
+    myScreenshot.save(PATH+'\\'+r'%s.png' % str(time.time()-START_TIME))
 
 
 def video_cap() -> None:
     cap = cv2.VideoCapture(0)
     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(
+        WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     while True:
         ret, img = cap.read()
         img = cv2.flip(img, 1)
         cv2.imshow(WINDOW_NAME, img)
+        pyautogui.click(100, 100)
         k = cv2.waitKey(1)
         if k == 27:
             exit()
@@ -34,7 +37,7 @@ def video_cap() -> None:
             window_capture()
 
 
-def get_window_pos(name):
+def get_window_pos(name) -> None:
     handle = win32gui.FindWindow(0, name)
     if handle == 0:
         return None
@@ -49,13 +52,43 @@ def fetch_image() -> None:
                          win32con.SC_RESTORE, 0)
     win32gui.SetForegroundWindow(handle)
     grab_image = ImageGrab.grab((x1, y1, x2, y2))
-    grab_image.save(THIS_PATH+'\\'+r'%s.png' % str(time.time()-START_TIME))
+    grab_image.save(PATH+'\\'+r'%s.png' % str(time.time()-START_TIME))
+
+
+def covered() -> str:
+    print('今天化身什么呢？')
+    print('1:顶碗人,2:贝极星,3:皇珈骑士, 4:嘉心糖, 5.奶淇淋')
+    user = input()
+    if user == '1':
+        covered = r".\source\bowl.png"
+    elif user == '2':
+        covered = r".\source\star.png"
+    elif user == '3':
+        covered = r".\source\knight.png"
+    elif user == '4':
+        covered = r".\source\candy.png"
+    elif user == '5':
+        covered = r".\source\icecream.png"
+    elif user == 'asoul':
+        covered = r".\source\A_soul.png"
+    elif user == 'mouse' or user == 'mice' or user == 'rat' or '鼠' in user:
+        covered = r".\source\mouse.png"
+    elif user == '毒唯' or user == 'dw':
+        covered = r".\source\joker.png"
+    elif user == 'carol' or user == 'Carol' or user == '珈乐':
+        covered = r".\source\Carol.png"
+    elif user == 'cr':
+        covered = r".\source\cr.png"
+    else:
+        covered = r".\source\ybb.png"
+    return covered
 
 
 if __name__ == '__main__':
-    joker = threading.Thread(target=scan.scan, args=())
-    video = threading.Thread(target=video_cap, args=())
-    video.start()
-    joker.start()
-    video.join()
-    joker.join()
+    result = covered()
+    p1 = Process(target=video_cap, args=())
+    p2 = Process(target=scan.scan, args=(result,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
